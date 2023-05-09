44b25df91ce313b84e55a7ec5e50511f60cad465
Philps <yunok96@naver.com>
2023-05-10 00:23:11 +0900

    최초커밋

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+
+  <modal-page 
+  v-bind:원룸들="원룸들"
+  v-bind:누른거="누른거"
+  v-bind:모달창="모달창"
+  />
+
+  <div class="menu">
+    <a v-for="a in menus" :key="a">{{ a }}</a>
+  </div>
+
+
+  <dis-ban></dis-ban>
+
+  <div v-for="(a, i) in 원룸들" :key="i">
+    <img :src="a.image" class="room-img">
+    <h4 v-on:click="모달창=true; 누른거=i">{{ a.title }}</h4>
+    <p>{{ a.content }}</p>
+    <p>{{ a.price }}</p>
+  </div>
+</template>
+
+<script>
+import data from './assets/data.js';
+import DiscountBanner from './components/DiscountBanner.vue';
+import Modal from './components/Modal.vue';
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      누른거:0,
+      원룸들:data,
+      모달창: false,
+      신고수 : 0,
+      menus: ['Home', 'Products', 'About'],
+      products: ['역삼동원룸', '천호동원룸', '강남동원룸']
+    }
+  },
+  methods : {
+    어쩌구() {
+      this.신고수 += 1;
+    },
+  },
+  components: {
+    DisBan : DiscountBanner,
+    ModalPage : Modal
+  }
+}
+
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.menu {
+  background-color: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+.room-img{
+  width: 100%;
+  margin-top: 40px;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="black-bg" v-if="모달창 == true">
+    <div class="white-bg">
+      <h4>{{ 원룸들[누른거].title }}</h4>
+      <p>{{ 원룸들[누른거].content }}</p>
+      <!-- <button v-on:click="모달창=false">모달창닫아</button> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'ModalPage',
+    props:{
+        원룸들:Array,
+        누른거:Number,
+        모달창:Boolean,
+    }
+}
+</script>
+
+<style>
+    .black-bg {
+        width: 100%; height: 100%;
+        background: rgba(0,0,0,0.5);
+        position: fixed; padding: 20px;
+    }
+    .white-bg {
+        width: 100%; background: white;
+        border-radius: 8px;
+        padding: 20px;
+    }
+</style>
